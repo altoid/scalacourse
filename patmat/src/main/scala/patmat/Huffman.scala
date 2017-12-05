fe6eac2387d0b0edd9d9711ead564c21a0c60605
Doug Tomm <dtomm@radiumone.com>
2017-12-05 09:10:23 -0800

    implemented weight and chars

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -18,15 +18,21 @@ object Huffman {
    * present in the leaves below it. The weight of a `Fork` node is the sum of the weights of these
    * leaves.
    */
-    abstract class CodeTree
+  abstract class CodeTree
   case class Fork(left: CodeTree, right: CodeTree, chars: List[Char], weight: Int) extends CodeTree
   case class Leaf(char: Char, weight: Int) extends CodeTree
   
 
   // Part 1: Basics
-    def weight(tree: CodeTree): Int = ??? // tree match ...
+    def weight(tree: CodeTree): Int = tree match {
+      case tree: Leaf => tree.weight
+      case tree: Fork => weight(tree.left) + weight(tree.right)
+    }
   
-    def chars(tree: CodeTree): List[Char] = ??? // tree match ...
+    def chars(tree: CodeTree): List[Char] = tree match {
+      case tree: Leaf => List(tree.char)
+      case tree: Fork => chars(tree.left) ++ chars(tree.right)
+    }
   
   def makeCodeTree(left: CodeTree, right: CodeTree) =
     Fork(left, right, chars(left) ::: chars(right), weight(left) + weight(right))
--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -14,7 +14,11 @@ class HuffmanSuite extends FunSuite {
 		val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
 	}
 
-
+  test("weight of a leaf") {
+    val l1 = Leaf('x', 42)
+    assert(weight(l1) === 42)
+  }
+  
   test("weight of a larger tree") {
     new TestTrees {
       assert(weight(t1) === 5)
@@ -29,23 +33,23 @@ class HuffmanSuite extends FunSuite {
   }
 
 
-  test("string2chars(\"hello, world\")") {
+  ignore("string2chars(\"hello, world\")") {
     assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
   }
 
 
-  test("makeOrderedLeafList for some frequency table") {
+  ignore("makeOrderedLeafList for some frequency table") {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
   }
 
 
-  test("combine of some leaf list") {
+  ignore("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
     assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
   }
 
 
-  test("decode and encode a very short text should be identity") {
+  ignore("decode and encode a very short text should be identity") {
     new TestTrees {
       assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
     }
